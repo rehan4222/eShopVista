d36d96e8bb395cc6dceed205ab66f0a9bbc945ba
Muhammad Rehan <mrh4222@outlook.com>
2024-10-06 11:45:14 -0700

    Small tweaks

--- source/service/routers/usersRouter.js
+++ source/service/routers/usersRouter.js
@@ -3,7 +3,7 @@ import { login, signup, getAll, deleteById, getById } from '../controllers/users
 import itemsRoute from './itemsRouter.js'
 import profileRouter from './profileRouter.js'
 import checkToken from '../middlewares/checkToken.js';
-import imageRouter from './imagesRouter.js';
+import picturesRouter from './picturesRouter.js';
 
 const router = Router();
 
@@ -17,7 +17,7 @@ router.delete('/:user_id', deleteById)
 
 router.use('/:user_id/items', checkToken, itemsRoute);
 router.use('/:user_id/profile', checkToken, profileRouter);
-router.use('/:user_id/images', imageRouter);
+router.use('/:user_id/images', picturesRouter);
 
 
 export default router;
